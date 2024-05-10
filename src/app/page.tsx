f08799194b7d00a21a16c28b6674af72d92bdedf
Harry MacCode <emarh.harry.code@gmail.com>
2024-05-10 19:19:20 +0200

    Installing NextUI and React icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import { Button } from '@nextui-org/react';
+import { FaRegSmile } from "react-icons/fa";
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-3xl text-red-500">Hello World !</h1>
+      <Button color='primary' variant='bordered' startContent={<FaRegSmile />}>Click Me</Button>
     </div>
   );
 }
