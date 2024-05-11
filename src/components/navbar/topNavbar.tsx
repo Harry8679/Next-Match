49f65d0fb0beec83d243ec60486f0bc3a43d0188
Harry MacCode <emarh.harry.code@gmail.com>
2024-05-11 09:48:48 +0200

    Making the link active

--- src/components/navbar/topNavbar.tsx
+++ src/components/navbar/topNavbar.tsx
@@ -1,10 +1,11 @@
 import { Button, Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@nextui-org/react";
 import Link from "next/link";
 import { GiMatchTip } from "react-icons/gi";
+import NavLink from "./NavLink";
 
 export default function TopNav() {
     return (
-        <Navbar maxWidth="xl"  className='bg-gradient-to-r from-purple-400 to-purple-700' classNames={{ item: ['text-xl', 'text-white', 'uppercase'] }}>
+        <Navbar maxWidth="xl"  className='bg-gradient-to-r from-purple-400 to-purple-700' classNames={{ item: ['text-xl', 'text-white', 'uppercase', 'data-[active=true]:text-yellow-300'] }}>
             <NavbarBrand as={Link} href='/'>
                 <GiMatchTip size={40} className='text-gray-200' />
                 <div className='font-bold text-3xl flex'>
@@ -15,9 +16,9 @@ export default function TopNav() {
             <NavbarContent justify="center">
 
             </NavbarContent>
-                <NavbarItem as={Link} href='/members'>Matches</NavbarItem>
-                <NavbarItem as={Link} href='/lists'>Lists</NavbarItem>
-                <NavbarItem as={Link} href='/messages'>Messages</NavbarItem>
+                <NavLink href="/members" label="Matches"/>
+                <NavLink href="/lists" label="Lists"/>
+                <NavLink href="/messages" label="Messages"/>
             <NavbarContent justify="end">
                 <Button variant="bordered" as={Link} href='/login' className="text-white">Login</Button>
                 <Button variant="bordered" as={Link} href='/register' className="text-white">Register</Button>
